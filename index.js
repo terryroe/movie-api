6ac74892472e85215fd866499b3fd3db4686f720
Terry Roe <terryroe@gmail.com>
2023-07-11 15:26:24 -0600

    use hashed password when updating user

--- index.js
+++ index.js
@@ -240,12 +240,13 @@ app.put(
       return res.status(422).json({ errors: errors.array() });
     }
 
+    const hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
         $set: {
           Username: req.body.Username,
-          Password: req.body.Password,
+          Password: hashedPassword,
           Email: req.body.Email,
           Birthday: req.body.Birthday,
         },
